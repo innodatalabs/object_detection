2e26657c9a315febb5ce8c5ae9d98dbb77f1a99b
Mike Kroutikov <mkroutikov@innodata.com>
2020-03-12 11:09:25 -0400

    add setup and requirements, readme updated

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # Sub-tree of tensorflow/models
 
-From https://github.com/tensorflow/models 
+From https://github.com/tensorflow/models
 
 1. copied `research/slim/` and `research/object_detection`
 2. added `requirements.txt`
 3. generated protocol buffer classes as per [object_detection/configure_api.md]
+4. added `requirements.txt` and `setup.py`
+5. generated package `python setup.py bdist_wheel` and pushed to the (private) pypi
+
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages, find_namespace_packages
+
+NAME = 'object_detection'
+
+setup(
+    name=NAME,
+    version='0.1.0',
+    description='See tensorflow/models on GitHub',
+
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    packages=find_packages(),
+    namespace_packages=[], #find_namespace_packages(),
+    install_requires=['tensorflow>=1.14.0', 'lxml>=4.5.0'],
+)
